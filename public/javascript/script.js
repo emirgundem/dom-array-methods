8b3378fe23e60c7e426741984cb2c231ddad9df5
emirgundem <emirgndem@gmail.com>
2022-01-23 21:13:17 +0300

    added sort by richest feature

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -23,14 +23,46 @@ async function getRandomUser(){
  const newUser = {
      name: `${user.name.first} ${user.name.last}`,
      money : Math.floor(Math.random() * 1000000)
-     
  }
  addData(newUser);
 }
 
+
+function doubleMoney(){
+    data = data.map((user)=>{
+        return { ...user , money:user.money * 2 }
+    })
+
+    updateDOM();
+}
+
+//Add new data 
 function addData(object){
     data.push(object);
     updateDOM();
+}
+
+//Sort by Richest
+function sortByRichest(){
+    data.sort((a,b)=>b.money-a.money);
+    updateDOM();
+
+}
+
+//Show Millionaries
+
+function showMillionaries(){
+    data = data.filter(user => user.money >1000000);
+    updateDOM();
+
+}
+
+
+
+//Format number as money
+function formatMoney(money){
+  return  money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + ' ₺';
+  
 }
 
 function updateDOM(providedData = data){
@@ -44,11 +76,9 @@ function updateDOM(providedData = data){
         });
 }
 
-//Format number as money
-function formatMoney(money){
-  return  money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + ' ₺';
-
-}
-
 
 addUserButton.addEventListener('click',getRandomUser);
+doubleMoneyButton.addEventListener('click',doubleMoney);
+sortButton.addEventListener('click',sortByRichest);
+showMillionairesButton.addEventListener('click',showMillionaries);
+
